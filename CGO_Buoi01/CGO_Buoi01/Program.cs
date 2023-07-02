8c9d9a83be45e9286370663ebb80934db28b1762
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-02 11:47:25 +0700

    Update bai tap loi chao

---
+++ CGO_Buoi01/CGO_Buoi01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+
+            Console.ReadKey();
+        }
+    }
+}
