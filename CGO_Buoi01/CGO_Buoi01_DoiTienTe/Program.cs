0d72f9fab650aa0e209d82c532e09f33e05eba47
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-02 12:02:14 +0700

    Update doi tien te

---
+++ CGO_Buoi01/CGO_Buoi01_DoiTienTe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi01_DoiTienTe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int rate = 23000;
+            Console.Write("Input your money in USD: ");
+            int money = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your money in VND is: " + (money * rate) + " VND");
+
+        }
+    }
+}
