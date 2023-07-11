3306ad2c1b285079d89f8a210afa135b72a08050
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-11 08:22:22 +0700

    Update tim max va vi tri

---
+++ CGO_Buoi4/CGO_Buoi04_TimMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi04_TimMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n: ");
+            int n = int.Parse(Console.ReadLine()); 
+            List<int> list = new List<int>();   
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Input element {0}: ",i);
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int maxValue = list.Max();
+            Console.WriteLine("Max Value is {0} at possition {1}", maxValue, list.IndexOf(maxValue));
+
+            Console.ReadKey();
+        }
+    }
+}
