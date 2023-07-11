df1da475cffed9c139dcad71a39b99b303f4e95a
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-11 09:04:34 +0700

    Update InsertElement

---
+++ CGO_Buoi4/CGO_Buoi04_InsertElement/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi04_InsertElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number of columns:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Enter the value for element at position [{i}, {j}]:");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matrix:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Input the number X you want to replace: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the row you want to replace: ");
+            int r = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the collumn you want to replace: ");
+            int c = int.Parse(Console.ReadLine());  
+
+            if (r > rows || c > columns)
+            {
+                Console.WriteLine("Cannot insert");
+            }
+            else
+            {
+                matrix[r, c] = x; 
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
