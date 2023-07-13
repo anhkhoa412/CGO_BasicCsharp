fc9a0eaf2134da28f7936f31a48d244e36e09a88
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-13 12:23:47 +0700

    Update Max in 2D array

---
+++ CGO_Buoi4/CGO_Buoi4_chengiatri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi4_chengiatri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number of n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Gia tri thu {0}= ", i);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            int value, index;
+            Console.Write("number you want to add: ");
+            value = int.Parse(Console.ReadLine());
+            Console.Write("Where do you want to add? ");
+            index = int.Parse(Console.ReadLine());  
+
+
+        }
+    }
+}
