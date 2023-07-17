b8460323b5c21917e9e5df2f6d918a648eea916c
anhkhoa412 <vunguyenanhkhoa12c8@gmail.com>
2023-07-17 10:58:36 +0700

    Update smaller Element

---
+++ CGO_Buoi05/CGO_Buoi05_TimMin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi05_TimMin
+{
+    internal class Program
+    {
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+        static void Main(string[] args)
+        {
+                int[] arr = { 1,2,4,2,3,1,4,5,9 };
+                int index = MinValue(arr);
+                Console.WriteLine("The smallest element in the array is: " + arr[index]);
+            
+        }
+    }
+}
